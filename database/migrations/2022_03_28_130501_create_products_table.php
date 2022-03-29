4d52ae230309afd5b8315cffaf376057e68fef72
jabber <2898117012@qq.com>
2022-03-29 10:15:41 +0800

    fix: database

--- database/migrations/2022_03_28_130501_create_products_table.php
+++ database/migrations/2022_03_28_130501_create_products_table.php
@@ -15,15 +15,25 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('slug')->unique()->nullable();
+            $table->string('name');
+            $table->string('url');
+            $table->decimal('price', 10, 2)->nullable(0);
+            $table->text('description');
+            $table->integer('score')->default(0);
             $table->timestamp('approved_at')->nullable();
+            $table->unsignedBigInteger('category_id');
             $table->unsignedBigInteger('country_id');
             $table->unsignedBigInteger('seller_id');
+            $table->unsignedBigInteger('brand_id')->nullable();
+            $table->unsignedBigInteger('platform_id')->nullable();
             $table->timestamps();
             $table->softDeletes();
+            $table->foreign('category_id')->references('id')->on('categories')->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('country_id')->references('id')->on('countries')->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('seller_id')->references('id')->on('sellers')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('brand_id')->references('id')->on('brands')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('platform_id')->references('id')->on('platforms')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
