94c22f2951175322864c1d2843746faf669b12cf
jabber <2898117012@qq.com>
2022-04-20 15:03:21 +0800

    init

---
+++ tests/Feature/MenuTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dealskoo\Product\Tests\Feature;
+
+use Dealskoo\Product\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+
+class MenuTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_menu()
+    {
+
+    }
+}
---
+++ tests/Feature/PermissionTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dealskoo\Product\Tests\Feature;
+
+use Dealskoo\Product\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+
+class PermissionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_permissions()
+    {
+
+    }
+}
